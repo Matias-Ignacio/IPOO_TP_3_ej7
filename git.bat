28f1f898a7bd3e078965b964d07985ab0eeeb00f
Matias-Ignacio <116597566+Matias-Ignacio@users.noreply.github.com>
2023-05-10 23:57:43 -0300

    Delete git.bat

--- git.bat
+++
@@ -1,7 +0,0 @@
-echo "# IPOO-TPs" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/Matias-Ignacio/IPOO-TPs.git
-git push -u origin main
